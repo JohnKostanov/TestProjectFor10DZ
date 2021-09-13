52c68e844f404554517fa81faf4c2d13096c5338
John Kostanov <47560053+JohnKostanov@users.noreply.github.com>
2021-09-13 18:06:13 +0200

    Initial Commit

---
+++ TestProjectFor10DZ/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestProjectFor10DZ
+//
+//  Created by Джон Костанов on 13/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestProjectFor10DZ/TestProjectFor10DZApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectFor10DZApp.swift
+//  TestProjectFor10DZ
+//
+//  Created by Джон Костанов on 13/9/21.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectFor10DZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
